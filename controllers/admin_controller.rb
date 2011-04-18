3f7e43147dbb1adc89e5e5f373eb8c25ffffa2c3
Brian Miller <brimil01@gmail.com>
2011-04-18 15:56:38 -0700

    Added admin panel view and admin acceptance tests

--- controllers/admin_controller.rb
+++ controllers/admin_controller.rb
@@ -1,21 +1,62 @@
 class Rstatus
-  get "/admin" do
+  before '/admin/*' do
     redirect "/" and return unless logged_in? && current_user.admin?
-    
   end
   
-  get '/admin/users' do
-    redirect "/" and return unless logged_in? && current_user.admin?
-    
+  get "/admin" do
+    @user_count = User.count
+    @users_new_24hours = User.count(["created_at > #{24.hours.ago}"])
+    @update_count = Update.count
+    @updates_24hours = Update.count(["created_at > #{24.hours.ago}"])
+    haml :"index"
   end
 
-  get '/admin/users/:username' do
-    redirect "/" and return unless logged_in? && current_user.admin?
-    
+  get '/admin/search/:username' do
+    @users = User.where(:username => /#{params[:username]}/i)
+    if @users.count == 1
+      @user = @users.first
+      if request.xhr? 
+        haml :"_user_profile", :layout => false
+      else
+        haml :"_user_profile"
+      end
+    else
+      if request.xhr? 
+        haml :"_user_list", :layout => false
+      else
+        haml :"_user_list"
+      end
+    end
+  end
+
+  get '/admin/user/:username' do
+    @user = User.first(:username => params[:username])
+    if request.xhr? 
+      haml :"_user_profile", :layout => false
+    else
+      haml :"_user_profile"
+    end
   end
   
-  get '/admin/users/:username/edit' do
-    redirect "/" and return unless logged_in? && current_user.admin?
-    
+  put '/admin/users/:username' do
+    @user = User.first(:username => params[:username])
+    if @user.edit_user_profile(params)
+      if request.xhr? 
+        "Profile saved!"
+      else
+        flash[:notice] = "Profile saved!"
+      end
+    else
+      if request.xhr? 
+        "Profile could not be saved!"
+      else
+        flash[:notice] = "Profile could not be saved!"
+      end
+    end
+  end
+  
+  delete '/admin/users/:username' do
+    @user = User.first(:username => params[:username])
+    @user.destroy
   end
 end
---
+++ test/acceptance/admin_test.rb
@@ -0,0 +1,10 @@
+require 'require_relative' if RUBY_VERSION[0,3] == '1.8'
+require_relative 'acceptance_helper'
+
+class RstatusAdminTest < MiniTest::Unit::TestCase
+
+  include AcceptanceHelper
+  
+  
+  
+end
